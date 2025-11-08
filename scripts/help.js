694e75cd3eef76bbdbbf58d4df5ec503453bb7e7
Miruna Agopian <agopian.miruna@yahoo.com>
2025-11-08 10:53:53 +0100

    added acctive class for legal notice and privacy policy for unlogged users on pc

--- scripts/help.js
+++ scripts/help.js
@@ -1,7 +1,28 @@
+window.addEventListener('DOMContentLoaded', checkCurrentUrl);
+
 function goBack() {
   window.history.back();
 }
 
-function setPolicyFocusTab(tab, deviceType) {
-  
+function setPolicyFocusTabPc(tab) {
+  let activeTabs = document.querySelectorAll('.aside-policy-links');
+  activeTabs.forEach(t => t.classList.remove('active'));
+  let idMap = {
+    privacyPolicy : 'privacy_policy_pc',
+    legalNotice: 'legal_notice_pc'
+  }
+  const activeTab = document.getElementById(idMap[tab]);
+  if(activeTab) activeTab.classList.add('active');
+}
+
+function checkCurrentUrl() {
+  const path = window.location.pathname.split('/').pop();
+  let urlMap = {
+    'privacy-policy-login.html' : 'privacyPolicy',
+    'legal-notice-login.html': 'legalNotice'
+  }
+  const tabKey = urlMap[path];
+  if(tabKey) {
+    setPolicyFocusTabPc(tabKey);
+  }
 }
