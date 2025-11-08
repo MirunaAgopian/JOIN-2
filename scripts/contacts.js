05e1fa42fddb4777dce30b751ebaf805684e741a
BEN <benbasic@outlook.de>
2025-11-08 16:38:28 +0100

    worked on dialog of contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -2,3 +2,27 @@ function openDialogAddContact(){
     const contentDialogContactRef = document.getElementById('add_contact_dialog');
     contentDialogContactRef.showModal();
 }
+
+function changeColorHover(){
+    const contentImageRef = document.getElementById('cancel_img');
+    const contentSpanRef = document.getElementById('cancel_span');
+
+    contentSpanRef.style = 'color : #29ABE2;';
+    contentImageRef.src = '../assets/img/cancel_x_hover.svg';
+}
+
+function changeColorOut(){
+    const contentImageRef = document.getElementById('cancel_img');
+    const contentSpanRef = document.getElementById('cancel_span');
+
+    contentSpanRef.style = 'color : #2A3647;';
+    contentImageRef.src = '../assets/img/cancel_x.svg';
+}
+
+function changeColorDown(){
+    const contentImageRef = document.getElementById('cancel_img');
+    const contentSpanRef = document.getElementById('cancel_span');
+
+    contentSpanRef.style = 'color : #091931;';
+    contentImageRef.src = '../assets/img/cancel_x_active.svg';
+}
