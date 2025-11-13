b12ba6fd0f614f22fe7ab5ac4fe1a985ab7878e8
Miruna Agopian <agopian.miruna@yahoo.com>
2025-11-13 20:01:13 +0100

    created functions to add tasks to firebase

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -1,3 +1,7 @@
+let tasks = [];
+let selectedPriority = null;
+const BASE_URL = 'https://remotestorage-162fc-default-rtdb.europe-west1.firebasedatabase.app/';
+
 function setPrioColor(element) {
   const containers = document.querySelectorAll('.txt-img');
   containers.forEach(c => {
@@ -8,6 +12,66 @@ function setPrioColor(element) {
   element.classList.add('active');
   const clickedIcon = element.querySelector('.prio-img');
   if (clickedIcon) clickedIcon.classList.add('active');
+  selectedPriority = selectPriority(element);
+}
+
+function selectPriority(element){
+  if(element.classList.contains('urgent')){
+    return 'urgent';
+  } else if(element.classList.contains('medium')) {
+    return 'medium';
+  } else {
+    return 'low';
+  }
+}
+
+function getTaskInput(){
+  return {
+    title: document.getElementById('task_title').value.trim(),
+    description: document.getElementById('task_description').value.trim(),
+    date: document.getElementById('due_date').value.trim(),
+    priority: selectedPriority,
+    assignedTo: document.getElementById('assigned_to').value,
+    category: document.getElementById('category').value,
+    subtasks: document.getElementById('subtask').value.trim()
+  };
+}
+
+function clearTaskInput(){
+  document.getElementById('task_title').value = '';
+  document.getElementById('task_description').value = '';
+  document.getElementById('due_date').value = '';
+  document.getElementById('assigned_to').value = '';
+  document.getElementById('category').value = '';
+  document.getElementById('subtask').value = '';
+  selectPriority = null;
+  document.querySelectorAll('.txt-img').forEach(c => c.classList.remove('active'));
+  document.querySelectorAll('.prio-img').forEach(icon => icon.classList.remove('active'));
+}
+
+function addTask(){
+  let task = getTaskInput();
+  tasks.push(task);
+  uploadTaskToFirebase('tasks', task)
+  .then(res => {
+    console.log('Task uploaded with ID:', res.name);
+    clearTaskInput();
+  })
+  .catch(err => {
+    console.error('Upload failed:', err)
+  });
+}
+
+async function uploadTaskToFirebase(path='', task={}) {
+  let response = await fetch( BASE_URL + path + '.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task)
+  });
+  let responseAsJson = await response.json();
+  return responseAsJson;
 }
 
 
