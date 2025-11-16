a03e541561f81477bfa6db631cd2fd6dde3daa3f
BEN <benbasic@outlook.de>
2025-11-16 15:55:00 +0100

    added contacs-dialog.js and .css to separate the content

---
+++ scripts/contacts-dialog.js
@@ -0,0 +1,142 @@
+function openDialogAddContact(){
+    const contentDialogContactRef = document.getElementById('add_contact_dialog');
+    contentDialogContactRef.showModal();
+    contentDialogContactRef.classList.add('dialogOpened');
+}
+
+async function closeDialogAddContact(){
+    const contentDialogContactRef = document.getElementById('add_contact_dialog');
+    contentDialogContactRef.classList.add('dialogClosed'); 
+    await timeout(600);
+    contentDialogContactRef.close();
+    contentDialogContactRef.classList.remove('dialogOpened');
+    contentDialogContactRef.classList.remove('dialogClosed');
+}
+
+function closeDialogAfterCreatedContact(){
+    const contentDialogContactRef = document.getElementById('add_contact_dialog');
+    contentDialogContactRef.close();
+    contentDialogContactRef.classList.remove('dialogOpened');
+}
+
+function getInputFields(){
+    let name = document.getElementById('add_name').value;
+    let mail = document.getElementById('add_mail').value;
+    let phone = document.getElementById('add_phone').value;
+
+    let inputData = createContactObj(name, mail, phone);
+    return inputData;
+}
+
+function createContactObj(name, mail, phone){
+    let contactObj = {
+        "name" : name,
+        "mail" : mail,
+        "phone" : phone
+    };
+    return contactObj;
+}
+
+function clearInputFields(){
+    document.getElementById('add_name').value = '';
+    document.getElementById('add_mail').value = '';
+    document.getElementById('add_phone').value = '';
+}
+
+function changeColorHover(){
+    const contentImageRef = document.getElementById('cancel_img');
+    const contentSpanRef = document.getElementById('cancel_span');
+
+    contentSpanRef.style = 'color : #29ABE2;';
+    contentImageRef.src = '../assets/img/cancel_x_hover.svg';
+}
+
+function changeColorOut(){
+    const contentImageRef = document.getElementById('cancel_img');
+    const contentSpanRef = document.getElementById('cancel_span');
+
+    contentSpanRef.style = 'color : #2A3647;';
+    contentImageRef.src = '../assets/img/cancel_x.svg';
+}
+
+function changeColorDown(){
+    const contentImageRef = document.getElementById('cancel_img');
+    const contentSpanRef = document.getElementById('cancel_span');
+
+    contentSpanRef.style = 'color : #091931;';
+    contentImageRef.src = '../assets/img/cancel_x_active.svg';
+}
+
+async function openContactMsgDialog(text){
+    closeDialogAfterCreatedContact();
+    const contentDialogRef = document.getElementById('msg_contact_dialog');
+    contentDialogRef.innerHTML = '';
+    contentDialogRef.innerHTML = getDialogMsgTemplate(text);
+    contentDialogRef.showModal();
+    contentDialogRef.classList.add('msg-opened');
+    await timeout(1000);
+    await closeContactMsgDialog(contentDialogRef);
+}
+
+async function closeContactMsgDialog(element){
+    const contentDialogRef = element;
+    contentDialogRef.classList.add('msg-closed');
+    await timeout(1000);
+    contentDialogRef.close();
+    contentDialogRef.classList.remove('msg-opened');
+    contentDialogRef.classList.remove('msg-closed');
+}
+
+function openDialogEditContact(){
+    const contentEditContactRef = document.getElementById('edit_contact_dialog');
+    contentEditContactRef.showModal();
+    contentEditContactRef.classList.add('dialogOpened');
+}
+
+async function closeDialogEditContact(){
+    const contentEditContactRef = document.getElementById('edit_contact_dialog');
+    contentEditContactRef.classList.add('dialogClosed'); 
+    await timeout(600);
+    contentEditContactRef.close();
+    contentEditContactRef.classList.remove('dialogOpened');
+    contentEditContactRef.classList.remove('dialogClosed');
+}
+
+function getInputFieldsEditDialog(){
+    let name = document.getElementById('edit_name').value;
+    let mail = document.getElementById('edit_mail').value;
+    let phone = document.getElementById('edit_phone').value;
+
+    let inputData = createContactEditObj(name, mail, phone);
+    return inputData;
+}
+
+function createContactEditObj(name, mail, phone){
+    let contactObj = {
+        "name" : name,
+        "mail" : mail,
+        "phone" : phone,
+        "color" : ''
+    };
+    return contactObj;
+}
+
+async function deleteContactOnDialog(mail){
+    let contactDeleted = false;
+    let contactsResponse = await getAllUsers('/contacts');
+    let contactsKeysArr = Object.keys(contactsResponse);
+    for (let index = 0; index < contactsKeysArr.length; index++) {
+        if((contactsResponse[contactsKeysArr[index]].mail) == mail){
+            await deleteData(`/contacts/${contactsKeysArr[index]}`);
+            contactDeleted = true;
+            break;
+        }
+    }
+    if(contactDeleted){
+        let dialog = document.getElementById('edit_contact_dialog');
+        dialog.close();
+        dialog.classList.remove('dialogOpened');
+        window.location.reload();
+    }
+}
+
--- scripts/contacts.js
+++ scripts/contacts.js
@@ -5,88 +5,10 @@ const colorContacts = ['#FF7A00', '#9327FF' , '#FF745E', '#FFC701', '#FFE62B',
 ];
 let activatedContact = 0;
 
-function openDialogAddContact(){
-    const contentDialogContactRef = document.getElementById('add_contact_dialog');
-    contentDialogContactRef.showModal();
-    contentDialogContactRef.classList.add('dialogOpened');
-}
-
-async function closeDialogAddContact(){
-    const contentDialogContactRef = document.getElementById('add_contact_dialog');
-    contentDialogContactRef.classList.add('dialogClosed'); 
-    await timeout(600);
-    contentDialogContactRef.close();
-    contentDialogContactRef.classList.remove('dialogOpened');
-    contentDialogContactRef.classList.remove('dialogClosed');
-}
-
-function closeDialogAfterCreatedContact(){
-    const contentDialogContactRef = document.getElementById('add_contact_dialog');
-    contentDialogContactRef.close();
-    contentDialogContactRef.classList.remove('dialogOpened');
-}
-
-function openDialogEditContact(){
-    const contentEditContactRef = document.getElementById('edit_contact_dialog');
-    contentEditContactRef.showModal();
-    contentEditContactRef.classList.add('dialogOpened');
-}
-
-async function closeDialogEditContact(){
-    const contentEditContactRef = document.getElementById('edit_contact_dialog');
-    contentEditContactRef.classList.add('dialogClosed'); 
-    await timeout(600);
-    contentEditContactRef.close();
-    contentEditContactRef.classList.remove('dialogOpened');
-    contentEditContactRef.classList.remove('dialogClosed');
-}
-
-function changeColorHover(){
-    const contentImageRef = document.getElementById('cancel_img');
-    const contentSpanRef = document.getElementById('cancel_span');
-
-    contentSpanRef.style = 'color : #29ABE2;';
-    contentImageRef.src = '../assets/img/cancel_x_hover.svg';
-}
-
-function changeColorOut(){
-    const contentImageRef = document.getElementById('cancel_img');
-    const contentSpanRef = document.getElementById('cancel_span');
-
-    contentSpanRef.style = 'color : #2A3647;';
-    contentImageRef.src = '../assets/img/cancel_x.svg';
-}
-
-function changeColorDown(){
-    const contentImageRef = document.getElementById('cancel_img');
-    const contentSpanRef = document.getElementById('cancel_span');
-
-    contentSpanRef.style = 'color : #091931;';
-    contentImageRef.src = '../assets/img/cancel_x_active.svg';
-}
-
 async function createContact(){
     let contactData = getInputFields();
     await createArrayOfContacts();
     await checkNewContact(contactData, joinContacts);
-}
-
-function getInputFields(){
-    let name = document.getElementById('add_name').value;
-    let mail = document.getElementById('add_mail').value;
-    let phone = document.getElementById('add_phone').value;
-
-    let inputData = createContactObj(name, mail, phone);
-    return inputData;
-}
-
-function createContactObj(name, mail, phone){
-    let contactObj = {
-        "name" : name,
-        "mail" : mail,
-        "phone" : phone
-    };
-    return contactObj;
 }
 
 async function createArrayOfContacts(){
@@ -124,15 +46,8 @@ async function checkNewContact(contactObj, contactsArr){
         clearInputFields();
     }
     else{
-        // alert('Kontakt schon vorhanden');
         await openContactMsgDialog('Contact already in list');
     }
-}
-
-function clearInputFields(){
-    document.getElementById('add_name').value = '';
-    document.getElementById('add_mail').value = '';
-    document.getElementById('add_phone').value = '';
 }
 
 async function postContactInDatabse(dataObj){
@@ -159,26 +74,6 @@ function getRandomColor(){
     let randomNumber = Math.floor(Math.random() * 15);
     color = colorContacts[randomNumber];
     return color;
-}
-
-async function openContactMsgDialog(text){
-    closeDialogAfterCreatedContact();
-    const contentDialogRef = document.getElementById('msg_contact_dialog');
-    contentDialogRef.innerHTML = '';
-    contentDialogRef.innerHTML = getDialogMsgTemplate(text);
-    contentDialogRef.showModal();
-    contentDialogRef.classList.add('msg-opened');
-    await timeout(1000);
-    await closeContactMsgDialog(contentDialogRef);
-}
-
-async function closeContactMsgDialog(element){
-    const contentDialogRef = element;
-    contentDialogRef.classList.add('msg-closed');
-    await timeout(1000);
-    contentDialogRef.close();
-    contentDialogRef.classList.remove('msg-opened');
-    contentDialogRef.classList.remove('msg-closed');
 }
 
 async function showClickedContact(mail){
@@ -240,25 +135,6 @@ async function deleteContact(mail){
     }
 }
 
-async function deleteContactOnDialog(mail){
-    let contactDeleted = false;
-    let contactsResponse = await getAllUsers('/contacts');
-    let contactsKeysArr = Object.keys(contactsResponse);
-    for (let index = 0; index < contactsKeysArr.length; index++) {
-        if((contactsResponse[contactsKeysArr[index]].mail) == mail){
-            await deleteData(`/contacts/${contactsKeysArr[index]}`);
-            contactDeleted = true;
-            break;
-        }
-    }
-    if(contactDeleted){
-        let dialog = document.getElementById('edit_contact_dialog');
-        dialog.close();
-        dialog.classList.remove('dialogOpened');
-        window.location.reload();
-    }
-}
-
 function renderEditDialog(mail, initials){
     let obj = joinContacts.find(c => c.mail == mail);
     const contentEditDialogRef = document.getElementById('edit_contact_dialog');
@@ -285,25 +161,6 @@ async function saveChangedData(mail){
         dialog.classList.remove('dialogOpened');
         window.location.reload();
     }
-}
-
-function getInputFieldsEditDialog(){
-    let name = document.getElementById('edit_name').value;
-    let mail = document.getElementById('edit_mail').value;
-    let phone = document.getElementById('edit_phone').value;
-
-    let inputData = createContactEditObj(name, mail, phone);
-    return inputData;
-}
-
-function createContactEditObj(name, mail, phone){
-    let contactObj = {
-        "name" : name,
-        "mail" : mail,
-        "phone" : phone,
-        "color" : ''
-    };
-    return contactObj;
 }
 
 function showActivatedContactInList(contactArr, contactNumber){
