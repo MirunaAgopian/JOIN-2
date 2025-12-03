1bc2eb3ce31b07dcc9f2cfcfd86f8e6b298225c2
Miruna Agopian <agopian.miruna@yahoo.com>
2025-12-03 10:44:15 +0100

    finished functionalities for summary.html

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,23 +1,9 @@
 
-//------------------------- Dynamic data summary.html ------------------------
-// /** const with Link for the Database */
-// const BASE_URL = 'https://remotestorage-162fc-default-rtdb.europe-west1.firebasedatabase.app/';
-
-// /** loads all useres that are stored in the database
-//  * @param {string} path key of the first Level of database
-//  * @returns json of the reqest  */
-// async function getAllUsers(path = '') {
-//     let response = await fetch(BASE_URL + path + '.json');
-//     let responseToJson = await response.json();
-
-//     return responseToJson;
-// }
-
 function onloadFunctionSummary(){
     renderActiveAvatar();
     insertNumbers();
-    insertPrioIcon();
     insertUpcomingDeadline();
+    insertDeadlineMessage();
 }
 
 function countTask(){
@@ -25,6 +11,7 @@ function countTask(){
     let countDone = 0;
     let countInProgress = 0;
     let countAwaitingFeedback = 0;
+    let countUrgentTasks = 0;
     for(let index = 0; index < todos.length; index++){
         if(todos[index].status == 'boardToDo'){
             countToDo++;
@@ -34,17 +21,21 @@ function countTask(){
             countInProgress++;
         } else if(todos[index].status == 'boardFeedback') {
             countAwaitingFeedback++;
-        } 
+        }
+        if(todos[index].priority == 'urgent' && todos[index].status !== 'boardDone'){
+            countUrgentTasks++;
+        }
     }
-    return returnNumberOfTasks(countToDo, countDone, countInProgress, countAwaitingFeedback);
+    return returnNumberOfTasks(countToDo, countDone, countInProgress, countAwaitingFeedback, countUrgentTasks);
 }
 
-function returnNumberOfTasks(toDo, done, inProgress, awaitingFeedback){
+function returnNumberOfTasks(toDo, done, inProgress, awaitingFeedback, urgent){
     return {
         toDo: toDo,
         done: done,
         inProgress: inProgress,
         awaitingFeedback: awaitingFeedback,
+        urgent: urgent,
         total: toDo + done + inProgress + awaitingFeedback
     };
 }
@@ -54,35 +45,15 @@ function insertNumbers(){
     let done = document.getElementById('done');
     let inProgress = document.getElementById('in_progress');
     let awaitingFeedback = document.getElementById('awaiting_feedback');
+    let urgentTasks = document.getElementById('urgent_tasks_number');
     let total = document.getElementById('total');
     let numbers = countTask();
     toDo.innerHTML = `${numbers.toDo}`;
     done.innerHTML = `${numbers.done}`;
     inProgress.innerHTML = `${numbers.inProgress}`;
     awaitingFeedback.innerHTML = `${numbers.awaitingFeedback}`;
+    urgentTasks.innerHTML = `${numbers.urgent}`
     total.innerHTML = `${numbers.total}`
-}
-
-function setPrioIcon(task) {
-    let background = document.getElementById('prio_icon_background');
-    let icon = document.getElementById('prio_icon');
-    let taskPriority = task.priority;
-    if(taskPriority == 'low'){
-        background.classList.add("prio-background", "low");
-        icon.classList.add("prio-background-img", "low");
-    } else if(taskPriority == 'medium'){
-        background.classList.add("prio-background", "medium");
-        icon.classList.add("prio-background-img", "medium");
-    } else if(taskPriority == 'urgent') {
-        background.classList.add('prio-background', 'urgent');
-        icon.classList.add('prio-background-img', 'urgent');
-    }
-}
-
-function insertPrioIcon() {
-    for(let index = 0; index < todos.length; index++){
-        setPrioIcon(todos[index]);
-    }
 }
 
 function getUpcomingDeadline(){
@@ -91,14 +62,14 @@ function getUpcomingDeadline(){
     for(let index = 0; index < todos.length; index++){
         let task = todos[index];
         let taskDate = new Date(task.date);
-        if(task.status !== 'boardDone' && taskDate > today){
+        if(task.status !== 'boardDone' && task.priority == 'urgent'  && taskDate > today){
             if(nextTask === null || taskDate < new Date(nextTask.date)){
                 nextTask = task;
             }
         }
     }
     return nextTask ? nextTask.date : null;
-} //wait for the answer of DA regadring which tasks count as deadline
+}
 
 function changeDateFormat(){
     let deadline = getUpcomingDeadline();
@@ -115,16 +86,21 @@ function insertUpcomingDeadline(){
     container.textContent = deadline; 
 }
 
-function insertNumberOfDeadlines(){
-    //.... to be continued
-    //...i also need to insert that status under the number according to the prio
+function insertDeadlineMessage(){
+    let deadlineString = getUpcomingDeadline();
+    let deadline = new Date(deadlineString);
+    let today = new Date();
+    let container = document.getElementById('deadline_message');
+    if(deadline < today) {
+        container.innerHTML = "Missed deadline";
+    } else {
+        container.innerHTML ="Upcoming deadline"
+    }
 }
 
 function redirectToBoard(){
     window.location.href = "./board.html"
 }
 
-//Next to do:
-// fix the console error regarding getAllUsers()
 
 
