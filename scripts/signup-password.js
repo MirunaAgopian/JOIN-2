fa4494f17189e9d082aad4aa7c4e50a6abb2fb2e
Patrick <patrick.sch@web.de>
2025-12-14 12:51:16 +0100

    reset input back to password on blur in signup

--- scripts/signup-password.js
+++ scripts/signup-password.js
@@ -1,5 +1,5 @@
 /**
- * html DOM definition
+ * HTML DOM definition for signup elements
  */
 let elementSignupRev = {
     password: document.getElementById("create_pw"),
@@ -9,33 +9,58 @@ let elementSignupRev = {
 }
 
 /**
- * EventListener for toggle icon of password
+ * Toggle the visibility of a password input field between "password" and "text".
+ *
+ * @param {HTMLInputElement} element - The input field whose type should be toggled.
+ */
+function togglePasswordInput(element) {
+    const isHidden = element.type === "password";
+    element.type = isHidden ? "text" : "password";
+    element.classList.remove("bg-img-lock");
+    if (isHidden) {
+        element.classList.remove("bg-img-invisible");
+        element.classList.add("bg-img-visible");
+    } else {
+        element.classList.remove("bg-img-visible");
+        element.classList.add("bg-img-invisible");
+    }
+}
+
+/**
+ * Reset the given input field back to type "password" and update its icon classes.
+ *
+ * @param {HTMLInputElement} element - The input field to reset.
+ */
+function resetInputToPassword(element) {
+    element.type = "password";
+    element.classList.remove("bg-img-visible");
+    element.classList.add("bg-img-invisible");
+}
+
+/**
+ * EventListener for toggle icon of password field
  */
 elementSignupRev.togglePassword.addEventListener("click", () => {
-    const isHidden = elementSignupRev.password.type === "password";
-    elementSignupRev.password.type = isHidden ? "text" : "password";
-    elementSignupRev.password.classList.remove("bg-img-lock");
-    if (isHidden) {
-        elementSignupRev.password.classList.remove("bg-img-invisible");
-        elementSignupRev.password.classList.add("bg-img-visible");
-    } else {
-        elementSignupRev.password.classList.remove("bg-img-visible");
-        elementSignupRev.password.classList.add("bg-img-invisible");
-    }
+    togglePasswordInput(elementSignupRev.password);
 });
 
 /**
- * EventListener for toggle icon of confirm password
+ * EventListener for toggle icon of confirm password field
  */
 elementSignupRev.toggleConfirmPassword.addEventListener("click", () => {
-    const isHidden = elementSignupRev.confirmPassword.type === "password";
-    elementSignupRev.confirmPassword.type = isHidden ? "text" : "password";
-    elementSignupRev.confirmPassword.classList.remove("bg-img-lock");
-    if (isHidden) {
-        elementSignupRev.confirmPassword.classList.remove("bg-img-invisible");
-        elementSignupRev.confirmPassword.classList.add("bg-img-visible");
-    } else {
-        elementSignupRev.confirmPassword.classList.remove("bg-img-visible");
-        elementSignupRev.confirmPassword.classList.add("bg-img-invisible");
-    }
+    togglePasswordInput(elementSignupRev.confirmPassword);
+});
+
+/**
+ * EventListener: reset password field when focus is lost
+ */
+elementSignupRev.password.addEventListener("blur", () => {
+    resetInputToPassword(elementSignupRev.password);
+});
+
+/**
+ * EventListener: reset confirm password field when focus is lost
+ */
+elementSignupRev.confirmPassword.addEventListener("blur", () => {
+    resetInputToPassword(elementSignupRev.confirmPassword);
 });
